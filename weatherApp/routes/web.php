b32e1f31819ae27c8e2a075556da4a2a08e9919e
alice <a.tel@st.hanze.nl>
2025-03-16 16:19:58 +0100

    forgot routes

--- weatherApp/routes/web.php
+++ weatherApp/routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
+use App\Http\Controllers\WeatherDataController;
+use App\Http\Controllers\WeatherViewController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
-
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/weather', [WeatherViewController::class, 'index'])->name('weather.index');
+Route::get('weather/dashboard', [WeatherViewController::class, 'dashboard'])->name('weather.dashboard');
+Route::get('weather/station/{name}', [WeatherViewController::class, 'stations'])->name('weather.station');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
