07985cc6ce18e2ddc46cc1e4d5cb1811eed509db
Tubsy <80598416+Tubsy@users.noreply.github.com>
2025-04-06 15:00:18 +0200

    Shows subscription types (barebones)

--- weatherApp/routes/web.php
+++ weatherApp/routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\WeatherDataController;
 use App\Http\Controllers\WeatherViewController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\RoleMiddleware;
+use App\Http\Controllers\SubscriptionTypeViewController;
 
 Route::get('/weather', [WeatherViewController::class, 'index'])->name('weather.index');
 Route::get('weather/dashboard', [WeatherViewController::class, 'dashboard'])->name('weather.dashboard');
@@ -13,6 +14,7 @@ Route::get('weather/station/{name}', [WeatherViewController::class, 'station'])-
 Route::get('weather/login', [WeatherViewController::class, 'loginPagina'])->name('weather.loginOefenen');
 Route::get('/', [WeatherViewController::class, 'home'])->name('home');
 
+Route::get('/subscriptions', [SubscriptionTypeViewController::class, 'index'])->name('subscriptions.index');
 
 Route::get('/register', [AuthController::class, 'showRegister'])->name('show.register');
 Route::post('/register', [AuthController::class, 'Register'])->name('register');
