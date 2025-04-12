6d7831d20758840bc47eaac71a0954a99da5a212
Job <163145054+job14142@users.noreply.github.com>
2025-04-12 12:55:24 +0200

    make query migration

--- weatherApp/database/migrations/2025_04_12_105215_create_iwa_subscription_query_table.php
+++ weatherApp/database/migrations/2025_04_12_105215_create_iwa_subscription_query_table.php
@@ -9,19 +9,23 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('iwa_subscription_query', function (Blueprint $table) {
             $table->id();
+            $table->string('query_id'); // Verwijst naar de query
+            $table->string('contract_identifier'); // Verwijst naar het contract
+            $table->json('landcodes')->nullable(); // Lijst van landcodes
+            $table->integer('elevation')->nullable(); // Elevatiecriteria
+            $table->json('coordinates')->nullable(); // Coördinatencriteria (bijvoorbeeld breedte- en lengtegraad)
+            $table->json('regions')->nullable(); // Lijst van regio's
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('iwa_subscription_query');
     }
+
 };
