e419aa6f4c3a861c710c83d281ef6ac732466f36
job14142 <163145054+job14142@users.noreply.github.com>
2025-04-15 10:20:50 +0200

    changed user rules now with "_"

--- weatherApp/database inserts/project_web_userroles.sql
+++ weatherApp/database inserts/project_web_userroles.sql
@@ -1,7 +1,7 @@
 USE `laravel`;
 
-LOCK TABLES `userroles` WRITE;
+LOCK TABLES `user_roles` WRITE;
 
-INSERT INTO `userroles` VALUES (1,'Technisch medewerker','Medewerker van de afdeling monitoring en beheer'),(2,'Technisch onderzoeker','Medewerker van de afdeling analyse en datamining'),(3,'Commercieel medewerker','Medewerker van de afdeling marketing en klant beheer'),(4,'Administratief medewerker','Medewerker van de afdeling personeelszaken'),(5,'Technisch beheerder','Medewerker van de afdeling IT-support'),(6,'Administrator','Super user');
+INSERT INTO `user_roles` VALUES (1,'Technisch medewerker','Medewerker van de afdeling monitoring en beheer'),(2,'Technisch onderzoeker','Medewerker van de afdeling analyse en datamining'),(3,'Commercieel medewerker','Medewerker van de afdeling marketing en klant beheer'),(4,'Administratief medewerker','Medewerker van de afdeling personeelszaken'),(5,'Technisch beheerder','Medewerker van de afdeling IT-support'),(6,'Administrator','Super user');
 
 UNLOCK TABLES;
