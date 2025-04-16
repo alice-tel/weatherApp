e0d857129713d5ef7e6aceffcedc9b074484deb8
job14142 <163145054+job14142@users.noreply.github.com>
2025-04-16 12:57:26 +0200

    hid pages behind login

--- weatherApp/resources/views/layouts/app.blade.php
+++ weatherApp/resources/views/layouts/app.blade.php
@@ -12,12 +12,7 @@
         <a class="navbar-brand" href="{{ route('home') }}">Weather Data</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('weather.dashboard') }}">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('weather.index') }}">Latest Data</a>
-                </li>
+
 
                 @foreach ($navItems ?? [] as $item)
                     <li class="nav-item">
@@ -26,9 +21,12 @@
                 @endforeach
 
                 @auth {{-- only visible when logged in (WORKS!) --}}
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('show.register') }}">Register</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('weather.dashboard') }}">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('weather.index') }}">Latest Data</a>
+                    </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('subscriptions.index') }}">Subscriptions</a>
                 </li>
