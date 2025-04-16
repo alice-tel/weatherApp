a49e401f6d77560a78a0eb06f29a2ef6ce5ab819
alice <a.tel@st.hanze.nl>
2025-04-16 15:40:48 +0200

    typo

--- weatherApp/resources/views/faulty/stations.blade.php
+++ weatherApp/resources/views/faulty/stations.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-12 mb-4">
             <div class="d-flex justify-content-between align-items-center">
-                <h1>Stations met foutive data</h1>
+                <h1>Stations met foutieve data</h1>
             </div>
         </div>
     </div>
